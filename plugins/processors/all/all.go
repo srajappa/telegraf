20ed71519471608c29e6bf0196ffdd5082baec5c
Philip Norman <philipnrmn@users.noreply.github.com>
2018-10-16 15:43:13 -0700

    Lowercase processor plugin (#26)
    * Add process plugin to enforce lowercase metrics
    
    * Cast names as well as fields to lowercase
    
    * Test that lowercase metrics are not duplicated
    
    With SendOriginals enabled, metrics with names or fields which contain uppercase
    characters should be duplicated. However, metrics which do not should not be.
    
    * Benchmark testing for uppercase chars
    
    * Factor out logic in Apply
    
    * Optimise the out slice

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -3,6 +3,7 @@ package all
 import (
 	_ "github.com/influxdata/telegraf/plugins/processors/converter"
 	_ "github.com/influxdata/telegraf/plugins/processors/dcos_metadata"
+	_ "github.com/influxdata/telegraf/plugins/processors/lowercase"
 	_ "github.com/influxdata/telegraf/plugins/processors/override"
 	_ "github.com/influxdata/telegraf/plugins/processors/printer"
 	_ "github.com/influxdata/telegraf/plugins/processors/regex"
