8bba59d0d884b3f00858d2d5730b240ffe69541f
Philip Norman <philipnrmn@users.noreply.github.com>
2018-12-07 15:48:39 -0800

    Prometheus mesos service discovery (#32)
    * Introduce Mesos flags to Prometheus plugin
    
    * Add test cases and infrastructure
    
    * Update tests with mesos service discovery
    
    * Add client retrieval to Mesos; stub URL collection
    
    * Implement service discovery for Mesos tasks
    
    * Update test to account for custom endpoints
    
    * Reference mesos in mock_mesos_server

---
+++ plugins/inputs/prometheus/mock_mesos_server.go
@@ -0,0 +1,68 @@
+package prometheus
+
+// NOTE: this file relies on protobuf fixtures. These are binary files and
+// cannot readily be changed. We therefore provide the go generate step below
+// which serializes the contents of json files in the testdata directory to
+// protobuf.
+//
+// You should run 'go generate' every time you change one of the json files in
+// the testdata directory, and commit both the changed json file and the
+// changed binary file.
+//go:generate go run cmd/gen.go
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+)
+
+// raw protobuf request types:
+// ref https://github.com/apache/mesos/blob/master/include/mesos/v1/agent/agent.proto
+var (
+	GET_CONTAINERS = []byte{8, 10}
+	GET_STATE      = []byte{8, 9}
+	GET_TASKS      = []byte{8, 13}
+)
+
+// startTestServer starts a server and serves the specified fixture's content
+// at /api/v1
+func startTestServer(t *testing.T, fixture string) *httptest.Server {
+	router := http.NewServeMux()
+	router.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+
+		w.Header().Set("Content-Type", "application/x-protobuf")
+		w.WriteHeader(http.StatusOK)
+		if bytes.Equal(body, GET_CONTAINERS) {
+			containers := loadFixture(t, filepath.Join(fixture, "containers.bin"))
+			w.Write(containers)
+			return
+		}
+		if bytes.Equal(body, GET_STATE) {
+			state := loadFixture(t, filepath.Join(fixture, "state.bin"))
+			w.Write(state)
+			return
+		}
+		if bytes.Equal(body, GET_TASKS) {
+			tasks := loadFixture(t, filepath.Join(fixture, "tasks.bin"))
+			w.Write(tasks)
+			return
+		}
+		t.Fatal("Body contained an unknown request: ", body)
+		return
+	})
+	return httptest.NewServer(router)
+}
+
+// loadFixture retrieves data from a file in ./testdata
+func loadFixture(t *testing.T, filename string) []byte {
+	path := filepath.Join("testdata", filename)
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return bytes
+}
